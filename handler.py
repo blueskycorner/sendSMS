ed5f02fb929878a952c876399b5a602d71b61aae
SESA401561 <SESA401561@W7FRR04774L.eur.gad.schneider-electric.com>
2018-01-08 12:12:05 +0100

    Intermediate step

--- handler.py
+++ handler.py
@@ -1,24 +1,57 @@
+import base64
 import json
+import os
+import urllib
+from urllib import request, parse
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+TWILIO_SMS_URL = "https://api.twilio.com/2010-04-01/Accounts/{}/Messages.json"
+TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
 
-    return response
+def sendSms(event, context):
+    to_number = event['To']
+    from_number = event['From']
+    body = event['Body']
+	
+    if not TWILIO_ACCOUNT_SID:
+        return "Unable to access Twilio Account SID."
+    elif not TWILIO_AUTH_TOKEN:
+        return "Unable to access Twilio Auth Token."
+    elif not to_number:
+        return "The function needs a 'To' number in the format +12023351493"
+    elif not from_number:
+        return "The function needs a 'From' number in the format +19732644156"
+    elif not body:
+        return "The function needs a 'Body' message to send."
+        
+    print("to_number : " + to_number)
+    print("from_number : " + from_number)
+    print("body : " + body)
+    
+    # insert Twilio Account SID into the REST API URL
+    populated_url = TWILIO_SMS_URL.format(TWILIO_ACCOUNT_SID)
+    post_params = {"To": to_number, "From": from_number, "Body": body}
+    print("populated_url : " + populated_url)
+    
+    # encode the parameters for Python's urllib
+    data = parse.urlencode(post_params).encode()
+    req = request.Request(populated_url)
+    
+    # add authentication header to request based on Account SID + Auth Token
+    authentication = "{}:{}".format(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+    base64string = base64.b64encode(authentication.encode('utf-8'))
+    req.add_header("Authorization", "Basic %s" % base64string.decode('ascii'))
+    #print("data : " + data.toString())
+    #print("req : " + req.toString())
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+    try:
+        # perform HTTP POST request
+        with request.urlopen(req, data) as f:
+            print("Twilio returned ")
+    except Exception as e:
+        print("something went wrong!")
+        return e
+
+    return "SMS sent successfully!"
